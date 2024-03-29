d3f950551fefe9f1d8434198c3f1d09e4cdb2fd9
mylifemyshiva <143156928+mylifemyshiva@users.noreply.github.com>
2024-03-29 03:43:36 +0000

    if and switch

--- java/controlflow.js
+++ java/controlflow.js
@@ -36,7 +36,62 @@ if (userLoggedin && debitCard) {
     console.log("allowed");
     
 }
+/* ---------- switch case
 
+syntex--
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+let months=13
+switch (months) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Not it the List");
+        break;
+}
 
 
 
